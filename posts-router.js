bac4d8cb32ef182000e06eafe9f17ded129a7868
Brian Bagrov <Brianbagrov@gmail.com>
2020-10-04 17:06:51 -0400

    MVP reached.

--- posts-router.js
+++ posts-router.js
@@ -4,9 +4,62 @@ const Posts = require('./data/db');
 
 router.post('/', (req, res) => {
 	const post = req.body;
-	Posts.insert(post)
-		.then((id) => {
-			res.status(201).json(id);
+
+	if (!post.title || !post.contents) {
+		res.status(400).json({
+			errorMessage: 'Please provide title and contents for the post.',
+		});
+	} else {
+		Posts.insert(post)
+			.then((id) => {
+				res.status(201).json(id);
+			})
+			.catch((err) => {
+				res.status(500).json(err);
+			});
+	}
+});
+
+router.get('/:id/comments', (req, res) => {
+	Posts.findPostComments(req.params.id)
+		.then((comments) => {
+			if (comments.length > 0) {
+				res.json(comments);
+			} else {
+				res.status(200).json({
+					message: 'This post has no comments yet',
+				});
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({
+				message: 'Could not get post comments',
+			});
+		});
+});
+
+router.post('/:id/comments', (req, res) => {
+	Posts.findById(req.params.id)
+		.then((post) => {
+			console.log(post);
+			if (post[0]) {
+				const newObj = {
+					text: req.body.text,
+					post_id: req.params.id,
+				};
+				Posts.insertComment(newObj)
+					.then((comment) => {
+						res.status(201).json(comment);
+					})
+					.catch((err) => {
+						res.status(500).json(err);
+					});
+			} else {
+				res
+					.status(404)
+					.json({ message: 'The post with the specified ID does not exist.' });
+			}
 		})
 		.catch((err) => {
 			res.status(500).json(err);
